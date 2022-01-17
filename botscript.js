4efebdaa49e9ca9f8811551cd6789c13960601a3
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2022-01-17 22:22:51 +1100

    Update botscript.js

--- botscript.js
+++ botscript.js
@@ -1,11 +1,27 @@
-const chatfield = document.getElementById("chat-input")
-const execbta = document.getElementById("chat-sound-btn")
+const ctrls = {
+  chatinp: document.getElementById("chat-input"),
+  chatbtn: document.getElementById("chat-send-btn"),
+  namebtn: document.getElementById("username-btn"),
+  nameokb: document.getElementById("username-ok-btn"),
+  nameinp: document.getElementById("username-box"),
+  usernam: document.getElementById("chat-user").innerHTML,
+  initpla: document.getElementById("chat-sound-btn")
+}
 const execbtn = "<button class='btn btn-default' onclick='execmsg();'>Executor</button>"
-
 function execmsg() {
   let cmdrun = prompt("Collabot Executor:")
-  if (cmdrun == "line") {
-    chatfield.document.write("This is a demo of Collabot.")
+  if (cmdrun == "test") {
+    ctrls.chatinp.value = "This is a demo of Collabot."
+    ctrls.chatbtn.click()
   }
 }
-execbta.innerHTML = execbtn
+ctrls.namebtn.click();
+ctrls.nameinp.value = "Collabot"
+ctrls.nameokb.click()
+ctrls.chatinp.value = "Collabot has been deployed by" + ctrls.usernam + "!"
+ctrls.chatbtn.click()
+ctrls.namebtn.click()
+ctrls.nameinp.value = ctrls.usernam
+ctrls.nameokb.click()
+
+ctrls.initpla.innerHTML = execbtn
