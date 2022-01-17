8c9c88439e87aaf195e427f7a9933e414ccae0f8
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2022-01-17 20:51:57 +1100

    Update botscript.js

--- botscript.js
+++ botscript.js
@@ -1,11 +1,11 @@
-const chatinnner = document.getElementById("chat-input").innerHTML
-const execbtp = document.getElementById("chat-sound-btn").innerHTML
+const chatfield = document.getElementById("chat-input")
+const execbta = document.getElementById("chat-sound-btn")
 const execbtn = "<button class='btn btn-default' onclick='execmsg();'>Executor</button>"
 
 function execmsg() {
   let cmdrun = prompt("Collabot Executor:")
   if (cmdrun == "line") {
-    chatinner.document.write("This is a demo of Collabot.")
+    chatfield.document.write("This is a demo of Collabot.")
   }
 }
-executor.document.write(execbtn)
+execbta.innerHTML = execbtn
